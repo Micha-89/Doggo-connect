c3788660eddfd20a4d986dafcf208716da483acc
Michal Raman <raman_michael@hotmail.com>
2021-02-15 22:08:22 +0100

    added dog profile

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const User = require('../models/User');
+const Dog = require('../models/Dog');
+
+router.get('/dogDetails/:id', (req, res) => {
+  const dogId = req.params.id;
+
+  Dog.findById(dogId)
+  .populate('shelter')
+  .then(dogInfo => {
+    console.log(dogInfo)
+    res.render('userViews/dogDetails', { dog : dogInfo})
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
+
+
+module.exports = router;
